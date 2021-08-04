553e96732a5b2ebd97f41c099ae63e5104b2e206
maxdesanta <maxdesanta1@gmail.com>
2021-08-04 11:49:35 +0700

    tugas pengadaan komputer max

---
+++ app.js
@@ -0,0 +1,18 @@
+// jumlah komputer
+function jumlahKomputer(kolom, baris) {
+    let totalSeluruhPc = kolom * baris;
+    return totalSeluruhPc;
+}
+
+// jumlah harga komputer
+function totalHargaKomputer(harga) {
+    let totalHargaPc = jumlahKomputer() * harga;
+    return totalHargaPc;
+}
+
+console.log(jumlahKomputer(8,10));
+console.log(100000);
+
+// hasil
+console.log("Max sedang membeli komputer sebanyak " + jumlahKomputer(8, 10) + " dengan total seharga : " + totalHargaKomputer(100000));
+
