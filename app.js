95583cfab254ba92e2c3944f3bf3adcd592bb14a
myairin <irenewindagultom@gmail.com>
2021-08-04 12:02:22 +0700

    tugas tiga belum selesai masi ada eror

---
+++ app.js
@@ -0,0 +1,13 @@
+//menghitung jumlah komputer
+function tampilkanKolomBaris(kolom, baris, harga) {
+    return kolom * baris * harga;
+}
+console.log(' komputer satu = ' + tampilkanKolomBaris(8, 10))
+console.log(' harga = ' + tampilkanKolomBaris(8, 10, 100000))
+console.log(' komputer dua = ' + tampilkanKolomBaris(4, 3))
+console.log(' harga = ' + tampilkanKolomBaris(4, 3, 152685))
+console.log(' komputer tiga = ' + tampilkanKolomBaris(15, 12))
+console.log(' harga = ' + tampilkanKolomBaris(15, 12, 305578))
+console.log(' komputer empat = ' + tampilkanKolomBaris(6, 8))
+console.log(' harga = ' + tampilkanKolomBaris(6, 8, 979567))
+
