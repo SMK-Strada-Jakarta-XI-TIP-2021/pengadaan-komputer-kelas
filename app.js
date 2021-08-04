4122e9c6b824050f2f336d5340ed23ea07f5657f
novi <novia@gmail.com>
2021-08-04 11:54:02 +0700

    tugas novi belum selesai

---
+++ app.js
@@ -0,0 +1,9 @@
+// fungsi menghitung jumlah komputer
+function jumlahKom (kolom, baris) {
+    return baris * kolom;
+}
+
+console.log(jumlahKom(8,10));
+console.log(jumlahKom(4,3));
+console.log(jumlahKom(15,12));
+console.log(jumlahKom(6,8));
