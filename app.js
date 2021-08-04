e7f3b6af647ed3abfc709a3fcfa4c159d84e844d
unknown <dwitegar611@gmail.com>
2021-08-04 12:31:33 +0700

    belum selesai

---
+++ app.js
@@ -0,0 +1,18 @@
+// code
+function jumlahKomputer (a, b) {
+    var baris;
+    var kolom;
+    var total;
+    
+    baris = a;
+    kolom = b;
+    
+    total = baris * kolom;
+    
+    return total;
+    
+    }
+    console.log ("Komputer 1" + " = " + jumlahKomputer(8, 10));
+    console.log ("Komputer 2" + " = " + jumlahKomputer(4, 3));
+    console.log ("Komputer 3" + " = " + jumlahKomputer(15, 12));
+    console.log ("Komputer 4" + " = " + jumlahKomputer(6, 8));
