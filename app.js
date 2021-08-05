040f901318c9477b1a48a47e52b73c074daeb96b
pthabot <prima.fams@gmail.com>
2021-08-05 22:36:04 +0700

    pengadaan-komputer-kelas prima

---
+++ app.js
@@ -0,0 +1,34 @@
+function jumlahKomputer (a, b) {
+    var baris;
+    var kolom;
+    var total;
+    
+    baris = a;
+    kolom = b;
+    
+    total = baris * kolom;
+    
+    return total;
+    
+    }
+    console.log ("Komputer 1" + " = " + jumlahKomputer(8, 10));
+    console.log ("Komputer 2" + " = " + jumlahKomputer(4, 3));
+    console.log ("Komputer 3" + " = " + jumlahKomputer(15, 12));
+    console.log ("Komputer 4" + " = " + jumlahKomputer(6, 8));
+    
+    function hargaKomputer (a , b) {
+        var harga;
+        var harga;
+        var total;
+    
+        harga = a ;
+        harga = b ;
+        total = a * b;
+    
+        return total;
+    }
+    
+    console.log('Harga Komputer' + ' 1 ' + ' = ' + hargaKomputer(80, 100000));
+    console.log('Harga Komputer' + ' 2 ' + ' = ' + hargaKomputer(12, 152685));
+    console.log('Harga Komputer' + ' 3 ' + ' = ' + hargaKomputer(180, 305578));
+    console.log('Harga Komputer' + ' 4 ' + ' = ' + hargaKomputer(48, 979567));
