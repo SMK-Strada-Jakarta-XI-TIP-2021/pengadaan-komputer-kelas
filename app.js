d268caf83a693f9097462eba995707aa29a81a1b
jlerapatty <lerapattyjoaquin@gmail.com>
2021-08-13 07:02:39 +0700

    Joaquin

---
+++ app.js
@@ -0,0 +1,30 @@
+// Jumlah Komputer
+function jumlahKomputer (kolom, baris) {
+    var baris, kolom, total;
+    baris, kolom  
+    total = baris * kolom;
+    
+    return total;
+    }
+
+    // Total Jumlah Komputer
+    console.log ("Komputer kelas 1 = " + jumlahKomputer(8, 10));
+    console.log ("Komputer kelas 2 = " + jumlahKomputer(4, 3));
+    console.log ("Komputer kelas 3 = " + jumlahKomputer(15, 12));
+    console.log ("Komputer kelas 4 = " + jumlahKomputer(6, 8));
+
+    console.log("")
+// Jumlah Harga Komputer
+ function hargaKomputer (harga , harga) {
+    var harga, harga, total;
+        harga, harga
+        total = harga * harga;
+    
+        return total;
+    }
+
+    // Total Harga Jumlah Komputer
+    console.log('Harga Komputer' + ' 1 ' + ' = ' + hargaKomputer(80, 100000));
+    console.log('Harga Komputer' + ' 2 ' + ' = ' + hargaKomputer(12, 152685));
+    console.log('Harga Komputer' + ' 3 ' + ' = ' + hargaKomputer(180, 305578));
+    console.log('Harga Komputer' + ' 4 ' + ' = ' + hargaKomputer(48, 979567));
