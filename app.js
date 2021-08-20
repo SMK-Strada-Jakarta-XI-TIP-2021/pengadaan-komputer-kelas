16a5d8bd0874056f2177622de217e82258ffb025
marstefano <stefano.2020@smk-strada.sch.id>
2021-08-20 08:40:27 +0700

    perubahan kesatu

---
+++ app.js
@@ -0,0 +1,28 @@
+// Jumlah Komputer
+function jumlahKomputer (kolom, baris) {
+    var baris, kolom, total;
+    baris, kolom  
+    total = baris * kolom;
+    
+    return total;
+    }
+    
+    // Total Jumlah Komputer
+    
+    console.log ("Komputer kelas 1 = " + jumlahKomputer(8, 10));
+    console.log ("Komputer kelas 2 = " + jumlahKomputer(4, 3));
+    console.log ("Komputer kelas 3 = " + jumlahKomputer(15, 12));
+    console.log ("Komputer kelas 4 = " + jumlahKomputer(6, 8));
+
+    console.log("")
+
+    // Jumlah Harga Komputer
+
+    function hargaKomputer (harga , harga) {
+    var harga, harga, total;
+        harga, harga
+        total = harga * harga;
+    
+        return total;
+    }
+
